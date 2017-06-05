1c4a9035ed0db9f4bc763a914a72eba51eec1700
root <root@localhost.localdomain>
2017-06-05 18:49:34 -0400

    1st script done

--- sh/non_determinism_test.sh
+++ sh/non_determinism_test.sh
@@ -1,7 +1,9 @@
-@cd ../bin
-@cat ../res/non_determinism_test
-@java -cp ".:../lib/commons-io-2.5.jar" Autoanalyze ../res/non_determinism_test
-@javac -cp ".:../lib/automaton.jar:../lib/commons-io-2.5.jar" ../res/non_determinism_test.java
-@rm ../res/non_determinism_test.java
-echo "\nRunning code ..."
-@java -cp ".:../lib/commons-io-2.5.jar:../lib/automaton.jar" non_determinism_test
+cd ../bin
+echo "--------------------"
+cat ../res/non_determinism_test
+echo "--------------------"
+java -cp ".:../lib/commons-io-2.5.jar" Autoanalyze ../res/non_determinism_test > /dev/null
+javac -d . -cp ".:../lib/automaton.jar:../lib/commons-io-2.5.jar" ../res/non_determinism_test.java
+rm ../res/non_determinism_test.java
+read -p "Press ENTER to run the code ..." key
+java -cp ".:../lib/commons-io-2.5.jar:../lib/automaton.jar" non_determinism_test
